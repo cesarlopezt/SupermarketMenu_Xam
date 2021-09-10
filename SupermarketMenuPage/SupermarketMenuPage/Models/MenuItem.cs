e27366f26546a25b493d3bf0707acef747079aa9
cesarlopezt <cesarlopez2808@gmail.com>
2021-09-10 01:15:51 -0400

    Add project files.

---
+++ SupermarketMenuPage/SupermarketMenuPage/Models/MenuItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SupermarketMenuPage.Models
+{
+    class MenuItem
+    {
+        public MenuItem(string title, string description, string image)
+        {
+            Title = title;
+            Description = description;
+            Img = image;
+        }
+        public string Title { get; }
+        public string Description { get; }
+        public string Img { get; }
+    }
+}
---
+++ SupermarketMenuPage/SupermarketMenuPage/ViewModels/MenuViewModel.cs
@@ -0,0 +1,30 @@
+﻿using SupermarketMenuPage.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace SupermarketMenuPage.ViewModels
+{
+    class MenuViewModel: INotifyPropertyChanged
+    {
+        public MenuItem  GeneralItem { get; }
+        public MenuItem  BabiesItem { get; }
+        public MenuItem  ClothesItem { get; }
+        public MenuItem  CelebrationItem { get; }
+        public MenuItem  PetsItem { get; }
+        public MenuItem CleaningItem { get; }
+
+        public MenuViewModel()
+        {
+            GeneralItem = new MenuItem("All", "1400 items", "cart");
+            BabiesItem = new MenuItem("Baby Items", "200 items", "child");
+            ClothesItem = new MenuItem("Clothes", "500 items", "clothes");
+            CelebrationItem = new MenuItem("Party Supplies", "250 items", "party");
+            PetsItem = new MenuItem("Pet Items", "150 items", "pets");
+            CleaningItem = new MenuItem("Cleaning Items", "300 items", "cleaning");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
